4003c00c2c32c6f0cc1a3648cfdb5822267954d2
Rituraj Ranjan <rituraj.ranjan.42@gmail.com>
2021-07-08 18:58:07 +0530

    added student login and register functionality and complaint submit

--- app/src/main/java/com/stee1ix/collegegrievancesystem/LoginActivity.java
+++ app/src/main/java/com/stee1ix/collegegrievancesystem/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.stee1ix.collegegrievancesystem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,14 +9,47 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SnapshotMetadata;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        //update UI with currentUser
     }
 
     private String getUserName() {
@@ -24,6 +58,82 @@ public class LoginActivity extends AppCompatActivity {
         return usName;
     }
 
+    private String getPassword() {
+        EditText password = findViewById(R.id.password);
+        String passwd = password.getText().toString();
+        return passwd;
+    }
+
+    private void createAccount(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("SignUp", "createUserWithEmail:success");
+
+                            //update db with user
+                            Map<String, String> student = new HashMap<>();
+                            student.put("email", email);
+//                            student.put("name", name)    put a name editText and import here
+
+                            DocumentReference documentReference = db.collection("students").document(mAuth.getUid());
+
+                            documentReference
+                                    .set(student).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void unused) {
+                                            Log.d("addUserData", "DocumentSnapshot added with ID: " + documentReference.getId());
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Log.w("addUserData", "Error adding document", e);
+                                        }
+                                    });
+                            //update UI with user
+                            Intent intent = new Intent(LoginActivity.this, StudentActivity.class);
+                            intent.putExtra("email", email);
+//                            intent.putExtras()
+                            startActivity(intent);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("SignUp", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                            //update UI with null
+                        }
+                    }
+                });
+    }
+
+    private void signIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //Sign in success, update UI with the signed users info
+                            Log.d("LogIn", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+
+                            //updateUI with user
+                            Intent intent = new Intent(LoginActivity.this, StudentActivity.class);
+                            intent.putExtra("Username", email);
+//                            intent.putExtras()
+                            startActivity(intent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("LogIn", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //update UI with null
+                        }
+                    }
+                });
+    }
 
     public void loginBtn(View view) {
         Button loginButton = findViewById(R.id.login_btn);
@@ -31,9 +141,9 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String userName = getUserName();
-                Intent intent = new Intent(LoginActivity.this, StudentActivity.class);
-                intent.putExtra("Username", userName);
-                startActivity(intent);
+                String password = getPassword();
+//                createAccount(userName, password);
+                signIn(userName,password);
             }
         });
     }
--- app/src/main/java/com/stee1ix/collegegrievancesystem/StudentActivity.java
+++ app/src/main/java/com/stee1ix/collegegrievancesystem/StudentActivity.java
@@ -1,30 +1,88 @@
 package com.stee1ix.collegegrievancesystem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class StudentActivity extends AppCompatActivity {
+    private FirebaseFirestore db;
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.student);
 
+        db = FirebaseFirestore.getInstance();
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
         //Dropdown logic
-        Spinner spinnerCatergory = findViewById(R.id.spinner_catergories);
-        ArrayAdapter<CharSequence>adapter = ArrayAdapter.createFromResource(this,
-                R.array.catergory, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerCatergory.setAdapter(adapter);
+//        Spinner spinnerCatergory = findViewById(R.id.spinner_catergories);
+//        ArrayAdapter<CharSequence>adapter = ArrayAdapter.createFromResource(this,
+//                R.array.catergory, android.R.layout.simple_spinner_item);
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinnerCatergory.setAdapter(adapter);
 
         //TextView to display the username
         TextView name = findViewById(R.id.display_name);
-        String userName = getIntent().getStringExtra("Username");
-        name.setText("Welcome, " + userName);
+        String email = getIntent().getStringExtra("email");
+        name.setText("Welcome, " + email);
+    }
 
+    public void submitComplaint(View view) {
+        EditText etSubject = findViewById(R.id.etSubject);
+        EditText etMessage = findViewById(R.id.etMessage);
+
+        String subject = etSubject.getText().toString();
+        String message = etMessage.getText().toString();
+
+
+        Map<String, String> complaint = new HashMap<>();
+        complaint.put(subject, subject);
+        complaint.put("message", message);
+
+        //update db with complaint
+        DocumentReference documentReference = db.collection("complaints").document(user.getUid());
+        documentReference
+                .update(subject, complaint)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.d("addUserData", "ComplaintDocumentSnapshot added with ID: " + documentReference.getId());
+                        etSubject.setText("");
+                        etMessage.setText("");
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("addUserData", "Error adding document", e);
+                    }
+                });
+    }
+
+    public void viewStudentHistory(View view) {
+        Intent intent = new Intent(this,StudentHistoryActivity.class);
+        startActivity(intent);
     }
 }
